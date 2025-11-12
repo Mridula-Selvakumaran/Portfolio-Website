6fe0f2c2ac11b1f0f417beeaf640e810acec3d70
Mridula-S-3006 <mridsk2k6@gmail.com>
2025-11-12 18:52:34 +0530

    React initialized and project structure built

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-indigo-900 to-black flex items-center justify-center">
+      <h1 className="text-white text-5xl font-bold">Tailwind is Working! Mri here.</h1>
+    </div>
+  );
+}
